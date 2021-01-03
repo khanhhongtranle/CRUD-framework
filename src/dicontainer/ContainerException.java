c75d0bb2d1fb44e4a390acfd3ba690bd08df13e7
Nguyen Quang Phuc <nguyenquangphuc.vn.hue@gmail.com>
2021-01-03 13:47:23 +0700

    IoC first commit

---
+++ src/dicontainer/ContainerException.java
@@ -0,0 +1,26 @@
+
+package dicontainer;
+
+
+public class ContainerException extends RuntimeException
+{
+    public ContainerException()
+    {
+        super("A ContainerException has been thrown");
+    }
+
+    public ContainerException(String message)
+    {
+        super(message);
+    }
+
+    public ContainerException(String message, Exception ex)
+    {
+        super(message, ex);
+    }
+
+    public ContainerException(Exception ex)
+    {
+        super(ex);
+    }
+}

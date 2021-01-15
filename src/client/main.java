324078cd492561e56d29c9d9959612083e554381
khanhhongtranle <khanhhong141049@gmail.com>
2021-01-16 00:01:33 +0700

    update main

--- src/client/main.java
+++ src/client/main.java
@@ -9,29 +9,6 @@ import java.sql.SQLException;
 
 public class main {
     public static void main(String[] args) {
-        //Framework framework = Framework.getInstance();
-//        //framework.membership();
-//        framework.connect("MYSQL", "localhost:3306", "root", "123456", "funretro");
-//        framework.form("users");
-//        framework.connect("MSSQL", "localhost:1433", "temp", "12345", "AdventureWorksDW2012");
-//        framework.form("FactFinance");
-
-        /*
-         framework with singleton pattern
-         */
-//        FrameworkV1 frameworkV1 = new FrameworkV1(Framework.getInstance());
-//        frameworkV1.connect("MYSQL", "localhost:3306", "root", "123456", "funretro");
-//        frameworkV1.createMembership();
-//       // frameworkV1.form("membership_users");
-//        if (frameworkV1.validateMember("admin1", "admin")){
-//            System.out.println("Is member");
-//        }else{
-//            System.out.println("Is not member");
-//        }
-
-        /*
-        framework with virtual proxy pattern
-         */
         ProxyFramework proxyFramework = new ProxyFramework();
         proxyFramework.connect(ProxyFramework.DatabaseType.MySQL, "localhost:3306", "root", "123456", "funretro");
         proxyFramework.form("boards");
@@ -40,5 +17,10 @@ public class main {
         frameworkV1.connect(ProxyFramework.DatabaseType.MySQL, "localhost:3306", "root", "123456", "funretro");
         frameworkV1.createMembership();
         frameworkV1.form("membership_users");
+        if (frameworkV1.validateMember("admin", "admin")){
+            System.out.println("Is a member");
+        }else{
+            System.out.println("Is not a member");
+        }
     }
 }
